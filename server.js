774ce3f404b7652c16bea3027016ed0c455d9b5d
Jordan Tang <jordantang0229@gmail.com>
2016-09-20 09:44:05 -0700

    added RESTful api model to server.js for finding, posting, and deleting

--- server.js
+++ server.js
@@ -34,3 +34,56 @@ console.log("App listening on port 8080");
 // routes ==================
 // api -------
 // get all todos
+app.get('/api/todos', function(req, res) {
+
+  // use mongoose to get all todos in DB
+  Todo.find(function(err, todos) {
+
+    //if error retrieving, send error
+
+    if(err) {
+      res.send(err);
+    }
+    res.json(todos); // return all todos in JSON format
+
+  });
+});
+
+// Create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+  // create a todo, information comes from AJAX request from Angular
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+      if(err)
+        res.send(err);
+
+      //get and return all the todos after you create another
+      Todo.find(function(err, todos) {
+        if(err)
+          res.send(err);
+
+        res.json(todos);
+      });
+  });
+});
+
+// Delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+      if(err)
+        res.send(err);
+
+      // get and return all the todos after you create another
+      Todo.find(function(err, todos) {
+        if(err)
+          res.send(err);
+
+        res.json(todos);
+      });
+  });
+});
